094cdc2a889543f8619cdd0d6315502bd2c8205f
Faisal Al Qasimi <falqas@users.noreply.github.com>
2018-03-14 14:54:32 -0700

    Create vowel-count.js
    add new vowel-count.js challenge

---
+++ challenges/vowel-count.js
@@ -0,0 +1,24 @@
+/*
+  Create a function `vowelCount` that accepts a string, and counts the number of times a vowel (a, e, i, o, u) occurs in the string. For example, given the word "applepie", vowelCount("applepie") should count all
+  occurrences of the vowels "a", "e", and "i", and then return the following output:
+  ```javascript
+  {
+    "a": 1,
+    "e": 2,
+    "i": 1
+  }
+  ```
+  Bonuses
+  - Make sure that lower case letters and upper case letters count for the same character.
+  - Ignore spaces, special characters, and punctuation.
+  - Add a single entry in your output for "non-vowels" (consontants), e.g.:
+    {
+      "a": 1,
+      "e": 2,
+      "i": 1,
+      "nonvowels": 4
+    }
+    ```
+*/
+
+// YOUR CODE HERE
